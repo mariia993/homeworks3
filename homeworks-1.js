9ab9ad32184700fdb0d0e5b512f5766b361089b6
mariia993 <104144789+mariia993@users.noreply.github.com>
2022-06-12 21:25:11 +0300

    Add files via upload

---
+++ homeworks-1.js
@@ -0,0 +1,18 @@
+// let result=1;
+let array=[2, 3, 4, 5];
+// for (let i=0; i<array.length; i++) {
+//     result*=array[i];
+// };
+// console.log(result);
+
+let result=1;
+let i=0;
+while (i < array.length) {
+    result*=array[i];
+    i++;
+};
+console.log(result);
+
+
+
+
---
+++ homeworks-while-1.js
@@ -0,0 +1,7 @@
+let result=1;
+let i=0;
+while (i < array.length) {
+    result*=array[i];
+    i++;
+};
+console.log(result);
