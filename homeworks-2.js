79959db8a5f33e7c2d1a89576f55e1dc227a5525
mariia993 <104144789+mariia993@users.noreply.github.com>
2022-06-12 21:30:27 +0300

    Add files via upload

---
+++ homeworks-2.js
@@ -0,0 +1,10 @@
+let array=[0, 15];
+for (let i=0; i<array.length; i++) {
+    if (i%2===0) {
+        console.log ("0 is even");
+    } else if (i%2===1) {
+        console.log ("1 is odd");
+    } else if (i%2===0) {
+        console.log ("2 is even");
+    };
+};
